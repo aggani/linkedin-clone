045220ee55ec8de99bf512fcbd2e657800d6b8a8
Anirudh <iamanirudhaggarwal@gmail.com>
2021-07-08 14:59:23 +0530

    Sidebar updated, Feed added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,6 +3,9 @@ import { Counter } from "../features/counter/Counter";
 import "./App.css";
 import Header from "./Header.jsx";
 import Sidebar from "./Sidebar.jsx";
+import Feed from "./Feed";
+
+
 function App() {
   return (
     <div className="app">
@@ -13,7 +16,7 @@ function App() {
       {/* {sidebar/left} */}
       <div className="app__body">
         <Sidebar />
-        {/* {Feed} */}
+        <Feed />
         {/* {widgets/right} */}
       </div>
     </div>
---
+++ src/Components/Feed.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import InputOptions  from "./InputOptions";
+import ImageIcon from "@material-ui/icons/Image";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+            <InputOptions Icon={ImageIcon} title='Photo' color="#70B5F9"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
